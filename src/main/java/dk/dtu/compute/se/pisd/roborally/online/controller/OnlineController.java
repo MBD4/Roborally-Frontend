1b3e3d07e92fda1ab3badd567ee1812ec524bc4c
MBD4 <mdragheim@protonmail.com>
2026-04-27 11:36:54 +0200

    makes sure create game post request has owner attached

--- src/main/java/dk/dtu/compute/se/pisd/roborally/online/controller/OnlineController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/online/controller/OnlineController.java
@@ -183,12 +183,13 @@ public class OnlineController {
 
 
             try {
-                // TODO Assignment 7b: Create the game (in the backend) with the config information
+                // DONE Assignment 7b: Create the game (in the backend) with the config information
                 //      provided in the game configuration
-                restClient.post().uri("/game").body(game).retrieve().body(Game.class);
                 // TODO Assignment 7c: Extend the game creation so that the currently signed in user
-                //      is the owener of the game, which should also be registered as the first
+                //      is the owner of the game, which should also be registered as the first
                 //      player of the game
+                game.setUser(onlineState.getSignedInUser());
+                restClient.post().uri("/game").body(game).retrieve().body(Game.class);
             } catch (Exception e) {
                 e.printStackTrace();
             }
